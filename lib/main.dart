4ec1ceb33a2ae1ddbe8a7f35a66b0d3e07186d2a
HuynSang2404 <2224802010365@student.tdmu.edu.vn>
2026-05-22 00:24:53 +0700

    Update Thống kê và Cài Đặt cùng điều chỉnh giao diện

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,7 @@ import 'features/calendar/add_event_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
